fefdeb71e97e2ba0b5f11743f25b1f1d2d102ed1
huen <huen@oristartech.com>
2021-09-01 09:49:26 +0800

    include dubbo sentinel

---
+++ web/src/main/java/org/example/config/SentinelAutoConfiguration.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SentinelAutoConfiguration {
+
+    @PostConstruct
+    public void init() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule("/*");
+        rule.setCount(1);
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        rule.setLimitApp("default");
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
